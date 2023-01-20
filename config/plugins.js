170ac2d97d06ff64d7b2777500cec0419d17d3b7
0xjdl <jdl.lourens@gmail.com>
2023-01-20 20:35:26 +0100

    strapione

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  // ...
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        product: {
+          field: 'slug',
+          references: 'NAME',
+        },
+      },
+    },
+  },
+  // ...
+});
